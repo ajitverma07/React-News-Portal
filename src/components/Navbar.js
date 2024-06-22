1cfd3de858dd8cc3ca832fe777d1371a3e3692c8
Ajit Verma <143831302+ajitverma07@users.noreply.github.com>
2024-06-22 12:54:08 +0530

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -15,7 +15,7 @@ const Navbar = () => {
   return (
     <nav className="navbar navbar-expand-lg navbar-light bg-warning">
       <div className="container-fluid">
-        <Link className="navbar-brand" to="/">News Portal</Link>
+        <Link className="navbar-brand" to="/">APTV</Link>
         <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
           <span className="navbar-toggler-icon"></span>
         </button>
